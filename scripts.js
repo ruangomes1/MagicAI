bf04bbae1115c46702220ba26af31e08555427b9
ruangomes1 <ruanalvesgomes26@gmail.com>
2025-07-05 11:22:57 -0300

    Novo início do projeto

---
+++ scripts.js
@@ -0,0 +1,24 @@
+let webhook = "https://ruangomes1.app.n8n.cloud/webhook/animacao-css"
+
+
+async function cliqueiNoBotao() {
+    let textoInput = document.querySelector(".input-animacao").value
+    let codigo = document.querySelector(".area-codigo")
+    let areaResultado = document.querySelector(".area-resultado")
+
+    let resposta = await fetch(webhook, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ pergunta: textoInput })
+    })
+
+    let resultado = await resposta.json()
+
+    let info = JSON.parse(resultado.resposta)
+
+    codigo.innerHTML = info.code
+
+    areaResultado.innerHTML = info.preview
+
+    document.head.insertAdjacentHTML('beforeend', "<style>"+ info.style +"</style>")
+}
